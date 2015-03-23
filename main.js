64836d73c823c92a4623d5de75b91f6106f40cbd
Andrei Neculaesei <andrew@algorithm.dk>
2015-03-23 19:19:46 +0100

    Module skeleton and updated commands

--- main.js
+++ main.js
@@ -6,10 +6,54 @@
 var RESERVED_KEYWORDS = [
   "list", "ls",
   "add",
-  "delete", "remove"
+  "remove", "rm",
+  "help"
 ];
-var ARGV = process.argv;
+var ARGV = process.argv.slice(2);
 var ARGC = ARGV.length;
 
 /* @end Globals */
 
+/* @start Main */
+
+if(ARGC == 0){
+	print_banner();
+} else {
+	run_command();
+}	
+
+/* @end Main */
+
+/* @start Banner */
+
+function print_banner(){
+  var banner = "BANNER";
+  console.log(banner);
+}
+
+/* @end Banner */
+
+/* @start Command interpreter */
+
+function run_command(){
+  switch(ARGC[1]){
+    case "add":
+      break;
+    case "remove":
+    case "rm":
+      break;
+    case "list":
+    case "ls":
+      break;
+    case "help":
+    default:
+      print_banner();
+      break;
+  }
+}
+
+/* @end Command interpreter */
+
+/* @start Command functions */
+
+/* @end Command functions */
